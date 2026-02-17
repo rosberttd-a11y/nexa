d2ba11eb620398eca583d5a7fefb62ecf277acdb
rosberttd-a11y <rosbert.td@gmail.com>
2026-02-17 11:58:08 -0400

    Delete nexa-vercel directory

--- nexa-vercel/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NexaLanding from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NexaLanding />
-  </React.StrictMode>,
-)
