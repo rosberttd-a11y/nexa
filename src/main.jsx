73eba78199153dcf1f817fed687fa0d660c2b3e6
rosberttd-a11y <rosbert.td@gmail.com>
2026-02-19 22:47:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NexaLanding from './App.jsx'
+import Gracias from './Gracias.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NexaLanding />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NexaLanding />} />
+        <Route path="/confirmacion" element={<Gracias />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
